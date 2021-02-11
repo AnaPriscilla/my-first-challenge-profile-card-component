820f97b79807488513738ba37903856ccaee4e27
Ana Priscilla <web.anapriscilla@gmail.com>
2021-02-11 03:31:13 -0300

    adicionei js

--- Desafio-02/a-minha-solucao-para-o-faq-accordion-card/arrow.js
+++ Desafio-02/a-minha-solucao-para-o-faq-accordion-card/arrow.js
@@ -1,21 +1,11 @@
-/* 
-Select the icon. Be careful when using querySelector because it will select
-the first element that matches the argument passed in. So if you have other images
-before the icon that you want to select then it might not work.
-In such a case, assign an `id` to your icon in HTML, e.g id="arrow-icon" and
-then you can select it as `document.querySelector('#arrow-icon')
-*/
-
+/* Usando querySelector */
 const img1 = document.querySelector('.arrow-1')
 const img2 = document.querySelector('.arrow-2')
 const img3 = document.querySelector('.arrow-3')
 const img4 = document.querySelector('.arrow-4')
 const img5 = document.querySelector('.arrow-5')
 
-/*
-Add a click event listener to your icon to toggle the class of `clicked` 
-above every time it is clicked 
-*/
+/* Adicionando o listener de evento de click */
 
 img1.addEventListener('click', () => {
     img1.classList.toggle('clicked')
